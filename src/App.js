7ece8a1051d536cfbb41814f8f33baeedcc691eb
Mrshivam007 <shivamsoniponda@gmail.com>
2022-10-04 11:09:59 +0530

    my-react card filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Search from './Search';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+import CoPresentIcon from '@mui/icons-material/CoPresent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Search />
+      <button type="button" class="btn btn-light" style={{ marginLeft: '100px', paddingTop: '156vh', marginTop: '-168vh', paddingLeft: '9vh', borderRadius: '0px' }}></button>
+      <HomeIcon style={{marginLeft: '-7vh', marginTop: '-124rem'}} />
+      <AccessibilityNewIcon style={{marginLeft: '-25px', marginTop: '-116rem'}} />
+      <PeopleIcon style={{marginLeft: '-22px', marginTop: '-108rem'}}/>
+      <CoPresentIcon style={{marginLeft: '-25px', marginTop: '-100rem'}} />
+      <button type="button" class="btn btn-light" style={{ marginLeft: '74rem', paddingTop: '156vh', marginTop: '-168vh', paddingLeft: '10vh', borderRadius: '0px' }}></button>
+    </>
   );
 }
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,67 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+
+
+
+const data = {
+
+    cardData:[
+        {
+            id:1,
+            img: img1,
+            title: 'car',
+            desc: 'desc 1'
+        },
+        {
+            id:2,
+            img: img2,
+            title: 'bike',
+            desc: 'desc 2'
+        },
+        {
+            id:3,
+            img: img3,
+            title: 'cycle',
+            desc: 'desc 3'
+        },
+        {
+            id:4,
+            img: img1,
+            title: 'truk',
+            desc: 'desc 4'
+        },
+        {
+            id:5,
+            img: img2,
+            title: 'air',
+            desc: 'desc 5'
+        },
+        {
+            id:6,
+            img: img3,
+            title: 'water',
+            desc: 'desc 6'
+        },
+        {
+            id:7,
+            img: img1,
+            title: 'solid',
+            desc: 'desc 7'
+        },
+        {
+            id:8,
+            img: img2,
+            title: 'gas',
+            desc: 'desc 8'
+        },
+        {
+            id:9,
+            img: img3,
+            title: 'liquid',
+            desc: 'desc 9'
+        }
+    ]
+
+}
+export default data;
